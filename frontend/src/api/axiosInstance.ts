84498ede123dfeac1cf4e00a3db1f0d803e49368
sbs-02 <dangolsaksham8@gmail.com>
2026-03-24 20:56:02 +0545

    fix: cors fix

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    // "http://localhost:8080/api" ||
+    "https://real-estate-portal-backend.vercel.app/api",
   withCredentials: true,
 });
 
